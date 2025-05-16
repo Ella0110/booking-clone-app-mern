f190d1d54c0f123a1a7640706931a0c78b980c04
Ella <ellawu010@gmail.com>
2025-05-16 17:32:24 +0800

    add hotel model and type

---
+++ backend/src/shared/type.ts
@@ -0,0 +1,16 @@
+export interface HotelType {
+    _id: string;
+    userId: string; // 创建酒店的 user
+    name: string;
+    city: string;
+    country: string;
+    description: string;
+    type: string; // 酒店的类型
+    adultCount: number; // 酒店房间可以容纳的成人人数
+    childCount: number;
+    facilities: string[];
+    pricePerNight: number;
+    starRating: number;
+    imageUrls: string[];
+    lastUpdated: Date;
+}
