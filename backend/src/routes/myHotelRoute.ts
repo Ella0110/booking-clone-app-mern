6095f5a4890bfd02909e5f5b56c9aa6523f113dc
Ella <ellawu010@gmail.com>
2025-05-17 12:17:24 +0800

    bugfix: req is empty due to validate before multer

--- backend/src/routes/myHotelRoute.ts
+++ backend/src/routes/myHotelRoute.ts
@@ -13,13 +13,12 @@ const upload = multer({
         fileSize: 5 * 1024 * 1024, // 5MB for max
     },
 });
-
 router.post(
     "/",
     verifyToken, // 验证登录，获取 userId
+    upload.array("imageFiles", 6), // 这里的 imageFiles 是前端表单名称，6 是 6 张图片
     hotelValidate, // express-validator 数据验证
     validate, // 获取上一步的报错信息，报 error
-    upload.array("imageFiles", 6), // 这里的 imageFiles 是前端表单名称，6 是 6 张图片
     createMyHotel
 );
 
