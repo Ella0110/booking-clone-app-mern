06c718c41c578b82a2b89cf1ee79a0eb4b07eea4
Ella <ellawu010@gmail.com>
2025-05-21 16:56:22 +0800

    bugfix: hotel can not find from hotel because page and limit wrong

--- backend/src/controllers/hotelController.ts
+++ backend/src/controllers/hotelController.ts
@@ -2,22 +2,29 @@ import { Request, Response, NextFunction } from "express";
 import catchAsync from "../utils/catchAsync";
 import Hotel from "../models/hotel";
 import { HotelSearchResponse } from "../shared/type";
+import AppError from "../utils/appError";
 
 export const getAllHotels = catchAsync(
     async (req: Request, res: Response, next: NextFunction) => {
-        const perPage = 5; // 每页有几个酒店
-        const page = parseInt(req.query.page ? req.query.page.toString() : "1"); // 当前在第几页
-        const skip = (perPage - 1) * page; // 跳过多少条酒店数据
+        const pageSize = 5; // 每页有几个酒店
+        const pageNumber = parseInt(
+            req.query.page ? req.query.page.toString() : "1"
+        ); // 当前在第几页
+        const skip = (pageNumber - 1) * pageSize; // 跳过多少条酒店数据
 
-        const hotelsList = await Hotel.find().skip(skip).limit(perPage);
-
+        const hotelsList = await Hotel.find().skip(skip).limit(pageSize);
+        if (!hotelsList.toString()) {
+            next(new AppError("Failed to find hotel!", 404));
+        }
+        console.log(!hotelsList.toString());
+        console.log(hotelsList);
         const totalItems = await Hotel.countDocuments(); // 酒店总量
         const response: HotelSearchResponse = {
             data: hotelsList,
             pagination: {
                 total: totalItems,
-                page: page,
-                pages: Math.ceil(totalItems / page),
+                page: pageNumber,
+                pages: Math.ceil(totalItems / pageSize),
             },
         };
         res.status(200).json(response);
