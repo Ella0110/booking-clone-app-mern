76d9f88933dce55a8a246a01672a8466d31d107e
Ella <ellawu010@gmail.com>
2025-05-21 16:56:57 +0800

    fetch searhHotel

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -7,7 +7,10 @@
 
 import type { RegisterFormData } from "./pages/Register";
 import type { SignInFormData } from "./pages/SignIn";
-import type { HotelType } from "../../backend/src/shared/type";
+import type {
+    HotelSearchResponse,
+    HotelType,
+} from "../../backend/src/shared/type";
 
 //前端导入 env 数据的方式
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
@@ -115,5 +118,36 @@ export const updateMyHotelById = async (hotelFormData: FormData) => {
     if (!response.ok) {
         throw new Error("Failed to fetch hotel by id"); // 如果报这个错说明后端出问题，不是前端
     }
+    return response.json();
+};
+
+export type SearchParams = {
+    destination?: string;
+    checkIn?: string;
+    checkOut?: string;
+    adultCount?: string;
+    childCount?: string;
+    page?: string;
+};
+
+export const searchHotels = async (
+    searchParams: SearchParams
+): Promise<HotelSearchResponse> => {
+    const queryParams = new URLSearchParams();
+    queryParams.append("destination", searchParams.destination || "");
+    queryParams.append("checkIn", searchParams.checkIn || "");
+    queryParams.append("checkOut", searchParams.checkOut || "");
+    queryParams.append("adultCount", searchParams.adultCount || "");
+    queryParams.append("childCount", searchParams.childCount || "");
+    queryParams.append("page", searchParams.page || "");
+
+    const response = await fetch(
+        `${API_BASE_URL}/api/hotels/search?${queryParams}`
+    );
+
+    if (!response.ok) {
+        throw new Error("Error searching hotels");
+    }
+
     return response.json();
 };
