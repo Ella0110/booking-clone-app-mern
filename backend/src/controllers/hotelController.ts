21a2b41f0f128205d1609cb8c86c73edc367e81c
Ella <ellawu010@gmail.com>
2025-05-21 19:52:41 +0800

    add filter and sort code to searhHotel api

--- backend/src/controllers/hotelController.ts
+++ backend/src/controllers/hotelController.ts
@@ -4,15 +4,92 @@ import Hotel from "../models/hotel";
 import { HotelSearchResponse } from "../shared/type";
 import AppError from "../utils/appError";
 
+const constructSearchQuery = (queryParams: any) => {
+    let constructedQuery: any = {};
+
+    if (queryParams.destination) {
+        constructedQuery.$or = [
+            { city: new RegExp(queryParams.destination, "i") },
+            { country: new RegExp(queryParams.destination, "i") },
+        ];
+    }
+
+    if (queryParams.adultCount) {
+        constructedQuery.adultCount = {
+            $gte: parseInt(queryParams.adultCount),
+        };
+    }
+
+    if (queryParams.childCount) {
+        constructedQuery.childCount = {
+            $gte: parseInt(queryParams.childCount),
+        };
+    }
+
+    if (queryParams.facilities) {
+        constructedQuery.facilities = {
+            $all: Array.isArray(queryParams.facilities)
+                ? queryParams.facilities
+                : [queryParams.facilities],
+        };
+    }
+
+    if (queryParams.types) {
+        constructedQuery.type = {
+            $in: Array.isArray(queryParams.types)
+                ? queryParams.types
+                : [queryParams.types],
+        };
+    }
+
+    if (queryParams.stars) {
+        const starRatings = Array.isArray(queryParams.stars)
+            ? queryParams.stars.map((star: string) => parseInt(star))
+            : parseInt(queryParams.stars);
+
+        constructedQuery.starRating = { $in: starRatings };
+    }
+
+    if (queryParams.maxPrice) {
+        constructedQuery.pricePerNight = {
+            $lte: parseInt(queryParams.maxPrice).toString(),
+        };
+    }
+
+    return constructedQuery;
+};
+
 export const getAllHotels = catchAsync(
     async (req: Request, res: Response, next: NextFunction) => {
+        // 过滤筛选
+        const query = constructSearchQuery(req.query);
+
+        // 排序
+        let sortOptions = {};
+        switch (req.query.sortOption) {
+            case "starRating":
+                sortOptions = { starRating: -1 }; // 星级降序
+                break;
+            case "pricePerNightAsc":
+                sortOptions = { pricePerNight: 1 };
+                break;
+            case "pricePerNightDesc":
+                sortOptions = { pricePerNight: -1 };
+                break;
+        }
+
+        // 分页
         const pageSize = 5; // 每页有几个酒店
         const pageNumber = parseInt(
             req.query.page ? req.query.page.toString() : "1"
         ); // 当前在第几页
         const skip = (pageNumber - 1) * pageSize; // 跳过多少条酒店数据
 
-        const hotelsList = await Hotel.find().skip(skip).limit(pageSize);
+        // 查数据库
+        const hotelsList = await Hotel.find(query)
+            .sort(sortOptions)
+            .skip(skip)
+            .limit(pageSize);
         if (!hotelsList.toString()) {
             next(new AppError("Failed to find hotel!", 404));
         }
