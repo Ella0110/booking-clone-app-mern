d952a283c7e615265cf0e4ccf98e8cac1721697a
Ella <ellawu010@gmail.com>
2025-06-09 15:40:27 +0800

    style(responsive): reduce font size on mobile view

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -2,10 +2,10 @@ const Hero = () => {
     return (
         <div className="bg-bookingblue py-16">
             <div className="container mx-auto flex flex-col gap-2">
-                <h1 className="text-5xl text-white tracking-wide font-extrabold">
+                <h1 className="text-3xl lg:text-5xl text-white tracking-wide font-extrabold">
                     Find your next stay
                 </h1>
-                <p className="text-2xl font-light tracking-wide text-white ">
+                <p className="text-lg lg:text-2xl font-light tracking-wide text-white ">
                     Search low prices on hotels, homes and much more...
                 </p>
             </div>
