d4448951eadc60af7c634e40e09193d87786ff5c
Ella <ellawu010@gmail.com>
2025-06-09 18:49:17 +0800

    style(responsive): reduce font size on mobile view

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,10 +2,10 @@ const Footer = () => {
     return (
         <div className="bg-bookingblue py-10">
             <div className="container mx-auto flex justify-between items-center">
-                <span className="text-3xl text-white font-bold tracking-tight">
+                <span className="text-xl md:text-3xl  text-white font-bold tracking-tight">
                     BookHub
                 </span>
-                <span className="text-white font-bold tracking-tight flex gap-4">
+                <span className="text-sm md:text-md text-white font-bold tracking-tight flex gap-4">
                     <p className="cursor-pointer">Privacy Policy</p>
                     <p className="cursor-pointer">Term of Service</p>
                 </span>
