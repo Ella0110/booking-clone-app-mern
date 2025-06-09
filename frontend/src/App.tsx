4379ec0f27037dae1ce5c5a9495f51ca45fcca42
Ella <ellawu010@gmail.com>
2025-06-09 15:39:01 +0800

    feat: add showSearchBar prop to LayoutSearch to toggle SearchBar display

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -33,7 +33,7 @@ const App = () => {
                 <Route
                     path="/detail/:hotelId"
                     element={
-                        <LayoutSearch>
+                        <LayoutSearch showSearchBar={false}>
                             <Detail />
                         </LayoutSearch>
                     }
--- frontend/src/layout/LayoutSearch.tsx
+++ frontend/src/layout/LayoutSearch.tsx
@@ -4,16 +4,21 @@ import SearchBar from "../components/SearchBar";
 
 interface Props {
     children: React.ReactNode;
+    showSearchBar?: boolean;
 }
 
-const LayoutSearch = ({ children }: Props) => {
+const LayoutSearch = ({ children, showSearchBar = true }: Props) => {
     return (
         <div className="flex flex-col min-h-screen">
             <Header />
             <div className="bg-bookingblue py-3"></div>
-            <div className="container mx-auto">
-                <SearchBar />
-            </div>
+
+            {showSearchBar && (
+                <div className="container mx-auto">
+                    <SearchBar />
+                </div>
+            )}
+
             <div className="container mx-auto py-10 flex-1">{children}</div>
             <Footer />
         </div>
