8352de93327901a80de493d04f774963ab437bec
Ella <ellawu010@gmail.com>
2025-06-09 18:30:44 +0800

    style(responsive): optimize search result card layout

--- frontend/src/components/SearchResultCard.tsx
+++ frontend/src/components/SearchResultCard.tsx
@@ -17,15 +17,15 @@ const SearchResultCard = ({ hotel }: Props) => {
             </div>
             {/* Hotel Detail */}
             <div className="flex flex-col gap-2">
-                <div className="flex justify-between ">
-                    <div className="flex items-center gap-2">
+                <div className="flex flex-col md:flex-row justify-between gap-1">
+                    <div className="flex flex-col md:flex-row  gap-1 md:gap-2">
                         <Link
                             to={`/detail/${hotel._id}`}
                             className="text-2xl font-bold tracking-wide text-bookingtext"
                         >
                             {hotel.name}
                         </Link>
-                        <div className="flex items-center">
+                        <div className="hidden md:flex items-center">
                             <span className="flex items-center">
                                 {Array.from({ length: hotel.starRating }).map(
                                     () => (
@@ -35,8 +35,8 @@ const SearchResultCard = ({ hotel }: Props) => {
                             </span>
                         </div>
                     </div>
-
-                    <div className="flex items-center gap-1 ">
+                    {/* 电脑端 */}
+                    <div className="hidden md:flex items-center gap-1 ">
                         <div className="flex flex-col">
                             <div className="font-semibold">{hotel.type}</div>
                             <div className="flex  justify-end text-xs text-gray-500">
@@ -45,6 +45,30 @@ const SearchResultCard = ({ hotel }: Props) => {
                         </div>
                         <div className="flex items-center px-3 py-1 font-bold text-white rounded-br-lg rounded-tl-lg rounded-tr-lg bg-bookingreviewiconbg">
                             {hotel.starRating}
+                        </div>
+                    </div>
+                    {/* 手机端 */}
+                    <div className="md:hidden flex items-center justify-between gap-2 ">
+                        <div className="flex items-center">
+                            <span className="flex items-center">
+                                {Array.from({ length: hotel.starRating }).map(
+                                    () => (
+                                        <AiFillStar className="text-amber-400" />
+                                    )
+                                )}
+                            </span>
+                        </div>
+
+                        <div className="md:hidden flex items-center justify-between gap-2">
+                            <div className="text-sm font-semibold">
+                                {hotel.type} ·
+                            </div>
+                            <div className="flex  justify-end text-sm text-gray-500">
+                                8 reviews
+                            </div>
+                            <div className="flex items-center text-sm px-2 py-1 font-bold text-white rounded-br-lg rounded-tl-lg rounded-tr-lg bg-bookingreviewiconbg">
+                                {hotel.starRating}
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -64,7 +88,7 @@ const SearchResultCard = ({ hotel }: Props) => {
                         </div>
                     </div>
                 </div>
-                <div className="flex justify-between mt-8">
+                <div className="flex flex-col  md:flex-row gap-4 md:justify-between mt-8">
                     <div className="flex gap-1 items-center">
                         {hotel.facilities.slice(0, 2).map((facility) => (
                             <span className="flex items-center font-semibold px-2 py-1 bg-bookingyellow text-bookingblue rounded-full text-xs whitespace-nowrap">
@@ -76,13 +100,15 @@ const SearchResultCard = ({ hotel }: Props) => {
                                 ` + ${hotel.facilities.length - 2} more`}
                         </span>
                     </div>
-                    <Link
-                        type="submit"
-                        to={`/detail/${hotel._id}`}
-                        className="bg-bookingbutton text-md text-white font-semibold px-3 py-1 rounded hover:bg-bookingbuttonhover disabled:bg-gray-400"
-                    >
-                        See availability
-                    </Link>
+                    <div className="">
+                        <Link
+                            type="submit"
+                            to={`/detail/${hotel._id}`}
+                            className="flex justify-center items-center  bg-bookingbutton text-md text-white font-semibold px-3 py-1 rounded hover:bg-bookingbuttonhover disabled:bg-gray-400"
+                        >
+                            See availability
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
