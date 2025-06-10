b00d3b982f3f25b75f916ab6462acbde080cbec2
Ella <ellawu010@gmail.com>
2025-06-10 17:03:16 +0800

    style: optimize searchResultCard mobile view

--- frontend/src/components/SearchResultCard.tsx
+++ frontend/src/components/SearchResultCard.tsx
@@ -49,6 +49,17 @@ const SearchResultCard = ({ hotel }: Props) => {
                     </div>
                     {/* 手机端 */}
                     <div className="md:hidden flex items-center justify-between gap-2 ">
+                        <div className="md:hidden flex items-center justify-between gap-2">
+                            <div className="flex items-center text-sm px-2 py-1 font-bold text-white rounded-br-md rounded-tl-md rounded-tr-md bg-bookingreviewiconbg">
+                                {hotel.starRating}
+                            </div>
+                            <div className="text-sm font-semibold">
+                                {hotel.type} ·
+                            </div>
+                            <div className="flex  justify-end text-sm text-gray-500">
+                                8 reviews
+                            </div>
+                        </div>
                         <div className="flex items-center">
                             <span className="flex items-center">
                                 {Array.from({ length: hotel.starRating }).map(
@@ -57,18 +68,6 @@ const SearchResultCard = ({ hotel }: Props) => {
                                     )
                                 )}
                             </span>
-                        </div>
-
-                        <div className="md:hidden flex items-center justify-between gap-2">
-                            <div className="text-sm font-semibold">
-                                {hotel.type} ·
-                            </div>
-                            <div className="flex  justify-end text-sm text-gray-500">
-                                8 reviews
-                            </div>
-                            <div className="flex items-center text-sm px-2 py-1 font-bold text-white rounded-br-lg rounded-tl-lg rounded-tr-lg bg-bookingreviewiconbg">
-                                {hotel.starRating}
-                            </div>
                         </div>
                     </div>
                 </div>
